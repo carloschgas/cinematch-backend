91b7bb76137614f921c413c12a9bab6f219fd5dc
carloschgas <139183141+carloschgas@users.noreply.github.com>
2026-01-05 23:20:13 -0300

    feat: first commit

---
+++ src/main/java/carloschgas/cinematch/CinematchApplication.java
@@ -0,0 +1,13 @@
+package carloschgas.cinematch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinematchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CinematchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/carloschgas/cinematch/CinematchApplicationTests.java
@@ -0,0 +1,13 @@
+package carloschgas.cinematch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinematchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
