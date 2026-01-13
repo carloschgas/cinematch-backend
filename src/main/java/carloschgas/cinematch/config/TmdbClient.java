54d86b12972a8db40f96b545f3ca1858b26fa620
carloschgas <139183141+carloschgas@users.noreply.github.com>
2026-01-13 00:55:19 -0300

    feat: logic to filter by streaming providers

--- src/main/java/carloschgas/cinematch/config/TmdbClient.java
+++ src/main/java/carloschgas/cinematch/config/TmdbClient.java
@@ -10,16 +10,50 @@ import java.util.List;
 
 @Component
 public class TmdbClient {
+
     private final RestTemplate restTemplate = new RestTemplate();
 
     @Value("${tmdb.api.key}")
     private String apiKey;
 
-    private static final String BASE_URL = "https://api.themoviedb.org/3/movie/popular?language=pt-BR";
+    private static final String POPULAR_URL =
+            "https://api.themoviedb.org/3/movie/popular";
+
+    private static final String MOVIE_URL =
+            "https://api.themoviedb.org/3/movie";
+
+    private static final String DISCOVER_MOVIE_URL =
+            "https://api.themoviedb.org/3/discover/movie";
 
     public List<MoviesDTO> fetchPopularMovies() {
+        String url = POPULAR_URL +
+                "?language=pt-BR&api_key=" + apiKey;
 
-        String url = BASE_URL + "&api_key=" + apiKey;
+        TmdbResults response =
+                restTemplate.getForObject(url, TmdbResults.class);
+
+        return response.results();
+    }
+
+    public MoviesDTO getMovieById(Long movieID) {
+        String url = MOVIE_URL +
+                "/" + movieID +
+                "?language=pt-BR&api_key=" + apiKey;
+
+        return restTemplate.getForObject(url, MoviesDTO.class);
+    }
+
+
+    public List<MoviesDTO> getMoviesByStreaming(List<Long> streamingID){
+
+        String providers = streamingID.stream().map(String::valueOf).reduce((a,b)->a+"|"+b).orElse("");
+
+        String url = DISCOVER_MOVIE_URL +
+                "?api_key=" + apiKey +
+                "&language=pt-BR" +
+                "&watch_region=BR" +
+                "&with_watch_providers=" + providers;
+
 
         TmdbResults response =
                 restTemplate.getForObject(url, TmdbResults.class);
