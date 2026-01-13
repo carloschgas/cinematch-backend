24b77327655221674433339110ff713926e95d64
carloschgas <139183141+carloschgas@users.noreply.github.com>
2026-01-13 00:52:44 -0300

    feat: updated movies service, logic to get movies by streaming provider

--- src/main/java/carloschgas/cinematch/services/MoviesService.java
+++ src/main/java/carloschgas/cinematch/services/MoviesService.java
@@ -2,6 +2,7 @@ package carloschgas.cinematch.services;
 
 import carloschgas.cinematch.DTOs.MoviesDTO;
 import carloschgas.cinematch.config.TmdbClient;
+import carloschgas.cinematch.entity.RoomEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -10,12 +11,39 @@ import java.util.List;
 public class MoviesService {
 
     private final TmdbClient OmbdbClient;
+    private final RoomService roomService;
 
-    public MoviesService(TmdbClient ombdbClient) {
+    public MoviesService(TmdbClient ombdbClient, RoomService roomService) {
         this.OmbdbClient = ombdbClient;
+        this.roomService = roomService;
     }
 
-    public List<MoviesDTO> searchMovies() {
+
+    public List<MoviesDTO> getMovies(String roomCode){
+
+        RoomEntity room = roomService.findByCode(roomCode);
+
+        if (room.getProvidersId().size() == 0){
+            return getPopularMovies();
+        }
+
+        else {
+            List<Long> streamingsId = room.getProvidersId().stream().toList();
+            return getMoviesByStreaming(streamingsId);
+        }
+
+    }
+
+    public List<MoviesDTO> getPopularMovies() {
         return OmbdbClient.fetchPopularMovies();
+    }
+
+
+    public MoviesDTO getMovieByID(Long movie_id){
+        return OmbdbClient.getMovieById(movie_id);
+    }
+
+    public List<MoviesDTO> getMoviesByStreaming(List<Long> streamingsId){
+        return OmbdbClient.getMoviesByStreaming(streamingsId);
     }
 }
